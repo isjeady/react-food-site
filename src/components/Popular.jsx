03cabb0f094b94e1bcf617b61af117fd0d134e97
Leandro <vitto.leandro@gmail.com>
2022-04-05 12:58:37 +0200

    First Service

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Popular = () => {
-  return (
-    <div>Popular</div>
-  )
+
+    const [popular,setPopular] = useState([]);
+    
+    useEffect(() => {
+        getPopular();
+    }, []);
+
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?number=10&tags=vegetarian,dessert&apiKey=${process.env.REACT_APP_API_KEY}`);
+
+        const data = await api.json();
+
+        setPopular(data.recipes);
+
+        console.log(data);
+    }
+
+    return <div>
+        {popular.map((item,idx) => {
+            return <div>
+                N:{idx+1}
+                <pre>
+                    <code>
+                        {JSON.stringify(item,undefined,2)}
+                    </code>
+                </pre>
+                <hr></hr>
+            </div>
+        })}
+    </div>
 }
 
 export default Popular;
