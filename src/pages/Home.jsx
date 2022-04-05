46f88d2b27aa36e58c6327cf9edd24e6c23e86a2
Leandro <vitto.leandro@gmail.com>
2022-04-05 12:43:13 +0200

    Structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Favorite from '../components/Favorite'
+import Popular from '../components/Popular'
+
+const Home = () => {
+  return (
+    <div>
+        Home
+        <Favorite />
+        <Popular />
+    </div>
+  )
+}
+
+export default Home;
