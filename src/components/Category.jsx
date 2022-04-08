85a6c6165aa38b6336c0df1f75d8be26fa1f90ac
Leandro Vitto <leandro.vitto@webscience.it>
2022-04-08 16:28:41 +0200

    Style Page

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -9,26 +9,26 @@ import styled from 'styled-components';
 const Category = () => {
   return (
     <List>
-        <NavLink to={"/"} >
+        <SLink to={"/"} >
             <FiHome />
             <h4>Home</h4>
-        </NavLink>
-        <NavLink to={"/cuisine/italian"} >
+        </SLink>
+        <SLink to={"/cuisine/italian"} >
             <FaPizzaSlice />
             <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={"/cuisine/american"} >
+        </SLink>
+        <SLink to={"/cuisine/american"} >
             <FaHamburger />
             <h4>American</h4>
-        </NavLink>
-        <NavLink to={"/cuisine/thai"} >
+        </SLink>
+        <SLink to={"/cuisine/thai"} >
             <GiNoodles />
             <h4>Thai</h4>
-        </NavLink>
-        <NavLink to={"/cuisine/japanese"} >
+        </SLink>
+        <SLink to={"/cuisine/japanese"} >
             <GiChopsticks />
             <h4>Japanese</h4>
-        </NavLink>
+        </SLink>
     </List>
   )
 }
@@ -36,7 +36,46 @@ const Category = () => {
 const List = styled.div`
     display: flex;
     justify-content: center;
-    maragin: 2rem 0rem;
+    margin: 2rem 0rem;
+`;
+
+const SLink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items:center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration:none;
+    background: linear-gradient(35deg, #66ffcc, #313131);
+    width: 6rem;
+    height: 6rem;
+    cursor:pointer;
+    transform: scale(0.7);
+
+    h4 {
+        color: white;
+        font-size: 0.9rem;
+    }
+
+    svg {
+        color: white;
+        font-size: 1.9rem;
+    }
+
+    &.active {
+        background: linear-gradient(35deg, #313131, #66ffcc);
+
+        h4 {
+            color: white;
+            font-size: 0.8rem;
+        }
+    
+        svg {
+            color: white;
+            font-size: 1.8rem;
+        }
+    }
 `;
 
 export default Category
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -2,16 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { getRecipesComplexSearchService } from '../service/recipes.service';
 import _ from 'lodash';
-
+import styled from "styled-components";
+import Title from '../components/ui/Title';
 
 const Cuisine = () => {
 
   const {type} = useParams();
   const [cuisine,setCuisine] = useState([]);
   
-  /* useEffect(() => {
+  useEffect(() => {
     getCuisine();
-  }, [type]); */
+  }, [type]);
 
   const getCuisine = async () => {
       const data = await getRecipesComplexSearchService(_.capitalize(type));
@@ -21,13 +22,41 @@ const Cuisine = () => {
   }
 
   return (
-    <div>
-        <hr></hr>
-        {_.capitalize(type)}
-        <hr></hr>
-        {JSON.stringify(cuisine)}
-    </div>
+    <>
+      <Title>{_.capitalize(type)}</Title>
+      <Grid>
+          {cuisine.map(item => {
+            return(
+              <Card key={item.id}>
+                <img src={item.image} alt="" />
+                <h4>{item.title}</h4> 
+              </Card>
+            );
+          })}
+      </Grid>
+    </>
   )
 }
+
+const Grid = styled.div`
+  display : grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem,1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+      border-radius: 2rem;
+      width : 100%,
+  }
+  a {
+      text-decoration: none;
+      width : 100%,
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 export default Cuisine;
